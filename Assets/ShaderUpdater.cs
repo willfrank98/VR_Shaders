844a661b3819def98ae3a6399a1f3d172f99d573
Tony Diep <anthony.tony.diep@gmail.com>
2019-12-11 13:03:54 -0700

    XRayShader
    Finished XRayShader and applied it to one of the funky drawers.

---
+++ Assets/ShaderUpdater.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shader : MonoBehaviour
+{
+	public GameObject[] gameObjects;
+	public Material[] materials;
+
+	private int matIndex = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+	// Update is called once per frame
+	void Update()
+	{
+		//if (Input.GetKeyDown(KeyCode.RightArrow))
+		//{
+		//	matIndex++;
+
+		//	if (matIndex >= materials.Length)
+		//	{
+		//		matIndex = 0;
+		//	}
+		//	foreach (GameObject obj in objectsToChangeMat)
+		//	{
+		//		obj.GetComponent<Renderer>().material = materials[matIndex];
+		//	}
+		//}
+	}
+}
